358ae99e0314d8e234bfe1f4ad5c84cf548f2b15
NightKnightBot <anand.panchdhari@gmail.com>
2023-04-04 02:09:32 +0530

    TRULLY final commit by Anand

--- php/reviews.php
+++ php/reviews.php
@@ -1,15 +1,26 @@
 <?php
-    $title = $_POST['title'];
+    $title = $_POST['title1'];
     $data = $_POST['data'];
-    $conn = new mysqli('localhost','root','' ,'review');
-    if($conn->connect_error){
-        echo "$conn->connect_error";
-        die("Connection Failed : " . $conn->connect_error); 
-    } else {
-        $stmt = $conn->prepare("insert into reviews(title,data) values(?, ?)");
-        $stmt->bind_param("ss", $title, $data);
-        $execval = $stmt->execute();
+    $rating = $_POST['rating'];
+    $conn = new mysqli('localhost','root','' ,'test') or die("Connection Failed : " . $conn->connect_error);
+      $stmt = $conn->prepare("insert into review(title,data,rating) values(?, ?, ?)") or die("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error);
+      $stmt->bind_param("ssi", $title, $data, $rating) or die("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+      $execval = $stmt->execute();
+      $stmt->close();
+    $conn->close();
+
+    // Check if the query executed successfully
+    if ($execval) {
         echo '<script>window.alert("done")</script>';
-        $stmt->close();
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            header("Location: ".$_SERVER['HTTP_REFERER']);
+        } else {
+            header("Location: /"); // Redirect to homepage if HTTP_REFERER is not set
+        }
+        exit();
+    } else {
+        // Print any errors that occurred during the query execution
+        echo "Error: " . $stmt->error;
     }
 ?>
+
